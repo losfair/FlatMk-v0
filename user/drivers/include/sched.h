a000f367d4c36ae70aa86dfab562133d636b46dd
losfair <zhy20000919@hotmail.com>
2020-01-02 11:27:52 +0800

    Busy wait if scheduler is not ready. Should be rare.

--- user/drivers/include/sched.h
+++ user/drivers/include/sched.h
@@ -9,7 +9,7 @@ static void sched_yield() {
     payload.data[0] = 0;
     fastipc_write(&payload);
 
-    if(TaskEndpoint_invoke(CAP_SCHED_YIELD) < 0) flatmk_throw();
+    while(TaskEndpoint_invoke(CAP_SCHED_YIELD) < 0);
 }
 
 static void sched_nanosleep(uint64_t ns) {
@@ -18,18 +18,7 @@ static void sched_nanosleep(uint64_t ns) {
     payload.data[1] = ns;
     fastipc_write(&payload);
 
-    if(TaskEndpoint_invoke(CAP_SCHED_YIELD) < 0) flatmk_throw();
-}
-
-static uint64_t sched_read_tsc_freq() {
-    struct FastIpcPayload payload = {0};
-    payload.data[0] = 2;
-    fastipc_write(&payload);
-
-    if(TaskEndpoint_invoke(CAP_SCHED_YIELD) < 0) flatmk_throw();
-    fastipc_read(&payload);
-    if((int64_t) payload.data[0] < 0) flatmk_throw();
-    return payload.data[0];
+    while(TaskEndpoint_invoke(CAP_SCHED_YIELD) < 0);
 }
 
 static int sched_create(struct TaskEndpoint endpoint) {
@@ -38,7 +27,7 @@ static int sched_create(struct TaskEndpoint endpoint) {
     fastipc_write(&payload);
 
     if(BasicTask_put_ipc_cap(CAP_ME, endpoint.cap, 1) < 0) flatmk_throw();
-    if(TaskEndpoint_invoke(CAP_SCHED_CREATE) < 0) flatmk_throw();
+    while(TaskEndpoint_invoke(CAP_SCHED_CREATE) < 0);
     fastipc_read(&payload);
     return (int) payload.data[0];
 }
