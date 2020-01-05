7d99bbb8eabce883f31cd0bb265b41c5fe92f7e0
losfair <zhy20000919@hotmail.com>
2020-01-05 15:25:28 +0800

    Remove old version of userspace library.

--- user/flatruntime-user/src/layout.rs
+++
@@ -1,3 +0,0 @@
-pub const HEAP_BASE: u64 = 0x7f0000000000;
-pub const STACK_END: u64 = 0x800000000000;
-pub const ELF_PAGE_TEMP_MAP_BASE: u64 = 0x7efffffff000;
--- user/init/src/image.rs
+++
@@ -1,6 +0,0 @@
-// Early usermode
-pub static SCHEDULER: &'static [u8] = include_bytes!("../../scheduler/target/x86_64-flatmk/release/scheduler");
-pub static SHMEMD: &'static [u8] = include_bytes!("../../shmemd/target/x86_64-flatmk/release/shmemd");
-
-// Late usermode
-pub static VGA: &'static [u8] = include_bytes!("../../vga/target/x86_64-flatmk/release/vga");
